05844b19d1d87774d0b77d26690c17d0d3f7fd25
Matt Marcoux <mattmarcoux@gmail.com>
2025-09-08 18:48:47 -0700

    qute, rofi, waybar, zathura

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -517,14 +517,14 @@ c.colors.webpage.darkmode.enabled = True
 # font setting, it's replaced with the fonts listed here. If set to an
 # empty value, a system-specific monospace default is used.
 # Type: List of Font, or Font
-c.fonts.default_family = 'Iosevka Nerd Font'
+c.fonts.default_family = 'JetBrainsMono Nerd Font'
 
 # Default font size to use. Whenever "default_size" is used in a font
 # setting, it's replaced with the size listed here. Valid values are
 # either a float value with a "pt" suffix, or an integer value with a
 # "px" suffix.
 # Type: String
-c.fonts.default_size = '16pt'
+c.fonts.default_size = '12pt'
 
 # Bindings for normal mode
 config.bind(',m', 'spawn mpv {url}')
@@ -830,3 +830,4 @@ c.colors.tabs.selected.even.fg = fg_default
 
 # Background color of selected even tabs.
 c.colors.tabs.selected.even.bg = bg_selection
+
